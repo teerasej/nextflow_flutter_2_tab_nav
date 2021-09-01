560792374e8cbbbb33f3758d72e30a2a8a04e8d7
Teerasej <teerasej@gmail.com>
2021-09-01 23:04:14 +0700

    first time finish

---
+++ android/app/src/main/kotlin/com/example/nextflow_flutter_2_tab_nav/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nextflow_flutter_2_tab_nav
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Nextflow Flutter Nav Tab Stack',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Contact'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          body: TabBarView(
+            children: [
+              Text('1'),
+              Text('2'),
+              Text('3'),
+            ],
+          ),
+          bottomNavigationBar: Material(
+            color: Colors.blue,
+            child: TabBar(
+              tabs: [
+                Tab(
+                  text: '1',
+                  icon: Icon(Icons.home),
+                ),
+                Tab(
+                  text: '1',
+                  icon: Icon(Icons.timeline),
+                ),
+                Tab(
+                  text: '1',
+                  icon: Icon(Icons.settings),
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+}
